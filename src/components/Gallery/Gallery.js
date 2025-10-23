74ed6ead64ca0b983b3a2f5ce0e64e4dd4ee2c3c
Taras Ozymok <taras.ozymok@kevychsolutions.com>
2025-10-23 17:37:32 +0300

    swiper added

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,4 +1,5 @@
-import { useEffect, useMemo, useState } from 'react';
+'use client';
+import { useEffect, useMemo, useState, useRef } from 'react';
 import styles from './styles.module.scss';
 
 const Gallery = ({ id, size }) => {
@@ -12,24 +13,67 @@ const Gallery = ({ id, size }) => {
     [id, size]
   );
 
-  const [selected, setSelected] = useState(images[0]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+  const [direction, setDirection] = useState('next');
+  const startX = useRef(0);
 
   useEffect(() => {
-    setSelected(images[0]);
-  }, [images[0]]);
+    setSelectedIndex(0);
+  }, [id, size]);
+
+  const handleTouchStart = (e) => {
+    startX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = (e) => {
+    const endX = e.changedTouches[0].clientX;
+    const diff = startX.current - endX;
+    if (Math.abs(diff) < 50 || animating) return;
+
+    if (diff > 0) {
+      handleChange('next');
+    } else {
+      handleChange('prev');
+    }
+  };
+
+  const handleChange = (dir) => {
+    setDirection(dir);
+    setAnimating(true);
+    setTimeout(() => {
+      setSelectedIndex((prev) =>
+        dir === 'next' ? (prev + 1) % images.length : (prev - 1 + images.length) % images.length
+      );
+      setAnimating(false);
+    }, 250);
+  };
 
   return (
-    <div>
-      <div className={styles.imageWrap}>
-        <img className={styles.image} src={selected} />
+    <div className={styles.gallery}>
+      <div
+        className={`${styles.imageWrap} ${animating ? styles.fadeOut : styles.fadeIn}`}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <img
+          key={images[selectedIndex]}
+          className={`${styles.image} ${
+            direction === 'next' ? styles.slideLeft : styles.slideRight
+          }`}
+          src={images[selectedIndex]}
+          alt="tree"
+        />
       </div>
+
       <div className={styles.imagesRow}>
-        {images.map((img) => (
+        {images.map((img, index) => (
           <img
             key={img}
-            onClick={() => setSelected(img)}
+            onClick={() => !animating && setSelectedIndex(index)}
             src={img}
-            className={selected == img ? styles.selectedSmallImage : styles.smallImage}
+            alt="thumbnail"
+            className={selectedIndex === index ? styles.selectedSmallImage : styles.smallImage}
           />
         ))}
       </div>
