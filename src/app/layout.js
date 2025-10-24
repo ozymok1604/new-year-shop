42bca8eb08e7ef170d0cd529635f9930d3a6e5eb
Taras Ozymok <taras.ozymok@kevychsolutions.com>
2025-10-24 16:53:21 +0300

    tiktok pixel added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const geistSans = Geist({
@@ -42,7 +43,50 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+
+        {/* ✅ TikTok Pixel через Next.js Script */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject = t;
+              var ttq = w[t] = w[t] || [];
+              ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie", "holdConsent", "revokeConsent", "grantConsent"];
+              ttq.setAndDefer = function (t, e) {
+                t[e] = function () {
+                  t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+                };
+              };
+              for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+              ttq.instance = function (t) {
+                for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]);
+                return e;
+              };
+              ttq.load = function (e, n) {
+                var r = "https://analytics.tiktok.com/i18n/pixel/events.js",
+                    o = n && n.partner;
+                ttq._i = ttq._i || {};
+                ttq._i[e] = [];
+                ttq._i[e]._u = r;
+                ttq._t = ttq._t || {};
+                ttq._t[e] = +new Date;
+                ttq._o = ttq._o || {};
+                ttq._o[e] = n || {};
+                n = d.createElement("script");
+                n.type = "text/javascript";
+                n.async = true;
+                n.src = r + "?sdkid=" + e + "&lib=" + t;
+                e = d.getElementsByTagName("script")[0];
+                e.parentNode.insertBefore(n, e);
+              };
+              ttq.load('D3TL3LRC77U53GBVVNPG');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
