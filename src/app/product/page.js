edf5ed245f68cd527f63b3c6f3fae6c83741b510
Taras Ozymok <taras.ozymok@kevychsolutions.com>
2025-10-24 11:55:44 +0300

    metadata replace

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,28 +1,6 @@
 import { Suspense } from 'react';
 import ProductContent from '@/components/ProductContent/ProductContent';
 
-export const metadata = {
-  title: 'Новий рік — Купити ялинку онлайн | New Year Shop',
-  description:
-    'Обери живу або штучну ялинку з доставкою по Україні. Найкраща якість і святковий настрій!',
-  keywords: ['ялинка', 'новий рік', 'декор', 'купити ялинку', 'штучна ялинка'],
-  openGraph: {
-    title: 'New Year Shop 🎄',
-    description: 'Обери ідеальну ялинку для свята!',
-    url: 'https://new-year-shop.vercel.app/',
-    siteName: 'New Year Shop',
-    images: [
-      {
-        url: '/baner.jpg',
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: 'uk_UA',
-    type: 'website',
-  },
-};
-
 export default function ProductPage() {
   return (
     <Suspense fallback={<>Loading...</>}>
